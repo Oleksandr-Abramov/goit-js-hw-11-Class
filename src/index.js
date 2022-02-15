44063d1399fe9412ec5d0b24f44248ebb489d790
Oleksandr Abramov <aleksander.abramov@gmail.com>
2022-02-15 17:13:54 +0200

    first work vers.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,88 @@
-import './sass/main.scss';
+import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import ApiService from './js/services/api-service';
+
+const refs = {
+  input: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
+
+const api = new ApiService();
+
+refs.input.addEventListener('submit', formGetInput);
+
+function formGetInput(e) {
+  e.preventDefault();
+  refs.gallery.innerHTML = '';
+  api.resetPage();
+  api.request = refs.input.searchQuery.value;
+  getInputApi();
+}
+
+async function getInputApi() {
+  const responseFromApi = await api.fetchImages();
+  console.log('~ responseFromApi', responseFromApi);
+
+  makeGallery(responseFromApi);
+  api.incrementPage();
+}
+function makeGallery(images) {
+  const totalHits = images.data.totalHits;
+  alerts(totalHits);
+  const arrayImg = images.data.hits;
+  const marcup = arrayImg
+    .map(
+      ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
+        `<a class="gallery__item" href="${largeImageURL}"><div class="photo-card"><div class="img-container"><img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" /></div><div class="info"><p class="info-item"><b>Likes</b><br>${likes}</p><p class="info-item"><b>Views</b><br>${views}</p><p class="info-item"><b>Comments</b><br>${comments}</p><p class="info-item"><b>Downloads</b><br>${downloads}</p></div></div></a>`,
+    )
+    .join('');
+  renderGallery(marcup);
+}
+const lightbox = new SimpleLightbox('.gallery__item');
+
+function renderGallery(markup) {
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+  pageScroll();
+  lightbox.refresh();
+}
+
+function alerts(totalHits) {
+  const totalImages = (api.page - 1) * 40;
+  if (totalHits === 0) {
+    return Notify.failure(
+      `Sorry, there are no images matching your search query. Please try again.`,
+    );
+  }
+
+  if (totalHits / totalImages <= 1) {
+    console.log('~ page', api.page);
+    return Notify.failure(`We're sorry, but you've reached the end of search results.`);
+  }
+
+  if (api.page === 1) {
+    return Notify.success(`Hooray! We found ${totalHits} images.`);
+  }
+}
+
+window.addEventListener('scroll', () => {
+  if (
+    window.scrollY + window.innerHeight - document.documentElement.scrollHeight < 1 &&
+    window.scrollY + window.innerHeight - document.documentElement.scrollHeight > -1
+  ) {
+    getInputApi();
+  }
+});
+
+function pageScroll() {
+  if (api.page === 1) {
+    const { height: formHeight } = refs.input.getBoundingClientRect();
+
+    window.scrollBy({
+      top: formHeight * 1.5,
+      behavior: 'smooth',
+    });
+  }
+}
---
+++ src/js/services/api-service.js
@@ -0,0 +1,42 @@
+class ApiService {
+  API_KEY = '25611286-d3301de9845eb7113c68c548e';
+  BASE_URL = 'https://pixabay.com/api/';
+  page = 1;
+  request = '';
+  static axios = require('axios').default;
+
+  set request(newRequest) {
+    this.request = newRequest;
+  }
+  //   constructor({ request }) {
+  //     this.request = request;
+  //   }
+
+  fetchImages() {
+    const params = new URLSearchParams({
+      key: this.API_KEY,
+      q: this.request,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 40,
+      page: this.page,
+    });
+    try {
+      return ApiService.axios.get(`${this.BASE_URL}?${params}`);
+    } catch (error) {
+      console.error('Ошибочка', error);
+    }
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+  get page() {
+    return this.page;
+  }
+}
+export default ApiService;
